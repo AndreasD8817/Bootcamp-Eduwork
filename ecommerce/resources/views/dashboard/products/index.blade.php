caad1d4c1f1737bcb98667d08e4507fd81604ec9
AndreasD8817 <andreas.damai.chrestianto@gmail.com>
2025-07-13 12:27:50 +0700

    update penambahan tugas 11

---
+++ ecommerce/resources/views/dashboard/products/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts')
+@section('title','Contoh Halaman Produk Dengan Resource Controller')
+@section('content')
+<h1>Contoh Halaman Produk Dengan Resource Controller</h1>
+@endsection
--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return 'Ini Route Utama';
 });
-Route::get('/products', function () {
-    return 'Ini Route products';
-});
+// Route::get('/products', function () {
+//     return 'Ini Route products';
+// });
 Route::get('/cart', function () {
     return 'Ini Route cart';
 });
 Route::get('/checkout', function () {
     return 'Ini Route checkout';
 });
+
+Route::resource('products',ProductController::class);
